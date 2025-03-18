33d86aa0cbc3b6c8d7c76b52225d299a739e7ff1
vex-spec <ruoroabigael@gmail.com>
2025-03-18 13:28:24 +0300

    second commit

---
+++ src/datatypes.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+
+    var x :Int = 55 //Int
+    var number:Double = 56.98 //Double
+    var mydecimal = 45.87F //Float
+    var school = "emobilis" //String
+    var grade = 'A' //Character
+    var isMammal = true //Boolean
+
+    println(x)
+    println(number)
+    println(mydecimal)
+    println(school)
+    println(grade)
+    println(isMammal)
+
+
+}
---
+++ src/introduction.kt
@@ -0,0 +1,8 @@
+fun main() {
+
+    println("Stacy")
+    println("eMobilis")
+    println(100)
+    println(20*3)
+
+}
