6d4c4aa5cd567a96aa7176bb15b76c3940ad473f
kirumbastacy25l <minniebooh7@gmail.com>
2025-03-24 20:14:48 +0300

    second commit

--- src/assignment.kt
+++ src/assignment.kt
@@ -1,1 +1,36 @@
+import java.util.Scanner
+
+
 //largest number 3numbers
+fun main() {
+
+    var read = Scanner(System.`in`)
+    println("Enter first number :")
+    var num1 = read.nextInt()
+
+    println("Enter second number:")
+    var num2 = read.nextInt()
+
+    println("Enter third number:")
+    var num3 = read.nextInt()
+
+
+    var largest = if (num1 >= num2 && num1 >= num3) {
+        num1
+    } else if (num2 >= num1 && num2 >= num3) {
+        num2
+    } else {
+        num3
+    }
+
+    println("The largest number is: $largest")
+
+
+
+
+
+
+
+
+
+}
